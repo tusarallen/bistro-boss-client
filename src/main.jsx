bc371aa995d4066cee1dbd0e01c05a833d200696
email <allentusar909@gmail.com>
2023-05-24 12:55:24 +0600

    learning dynamic page title

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="w-11/12 mx-auto">
-      <RouterProvider router={router} />
-    </div>
+    <HelmetProvider>
+      <div className="w-11/12 mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
   </React.StrictMode>
 );
