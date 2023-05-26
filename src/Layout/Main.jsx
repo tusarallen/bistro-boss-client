00687bce421f5e6eebe49d2043ee016c8cdc24c0
email <allentusar909@gmail.com>
2023-05-26 20:51:29 +0600

    add some functionality

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import NavBar from "../pages/Shared/NavBar/NavBar";
 
 const Main = () => {
+  const location = useLocation();
+  const noHeaderFooter = location.pathname.includes("login");
+  
   return (
     <div>
-      <NavBar />
+      {noHeaderFooter || <NavBar />}
       <Outlet />
-      <Footer />
+      {noHeaderFooter || <Footer />}
     </div>
   );
 };
