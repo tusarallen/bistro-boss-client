a2462db313032506dab52712855e017283246611
email <allentusar909@gmail.com>
2023-05-27 16:42:47 +0600

    validation sugnup with react hook

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,7 @@ import {
 } from "react-simple-captcha";
 import { AuthContext } from "../../providers/AuthProvider";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Login = () => {
   const { signIn } = useContext(AuthContext);
@@ -38,80 +39,88 @@ const Login = () => {
   };
 
   return (
-    <div className="hero min-h-screen">
-      <div className="hero-content flex-col md:flex-row-reverse">
-        <div className="text-center md:w-1/2 lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card md:w-1/2 flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleLogin} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="password"
-                className="input input-bordered"
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <LoadCanvasTemplate />
-              </label>
-              <input
-                ref={captchaRef}
-                type="text"
-                name="captcha"
-                placeholder="type the text captcha avobe"
-                className="input input-bordered"
-              />
-              <button
-                onClick={handleCaptcha}
-                className="btn btn-outline btn-xs mt-2"
-              >
-                Validate
-              </button>
-            </div>
-            <div className="form-control mt-4">
-              <input
-                disabled={disabled}
-                className="btn btn-primary"
-                type="submit"
-                value="Login"
-              />
-            </div>
-          </form>
-          <p className="md:-mt-5 p-3">
-            <small>
-              New Here? <Link className="underline font-bold text-[green]" to="/signup">Create an account</Link>
-            </small>
-          </p>
+    <>
+      <Helmet>
+        <title>Bistro Boss | Sign Up</title>
+      </Helmet>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col md:flex-row-reverse">
+          <div className="text-center md:w-1/2 lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card md:w-1/2 flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <LoadCanvasTemplate />
+                </label>
+                <input
+                  ref={captchaRef}
+                  type="text"
+                  name="captcha"
+                  placeholder="type the text captcha avobe"
+                  className="input input-bordered"
+                />
+                <button
+                  onClick={handleCaptcha}
+                  className="btn btn-outline btn-xs mt-2"
+                >
+                  Validate
+                </button>
+              </div>
+              <div className="form-control mt-4">
+                <input
+                  disabled={disabled}
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
+            <p className="md:-mt-5 p-3 text-xl">
+              <small>
+                New Here?{" "}
+                <Link className="underline font-bold text-[green]" to="/signup">
+                  Create an account
+                </Link>
+              </small>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
